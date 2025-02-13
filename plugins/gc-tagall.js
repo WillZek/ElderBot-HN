ac8ce97a868a67cb2b18dffe8ca2a0c65b7d6d65
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-13 12:46:28 -0600

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,8 +6,9 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, co
 
   let api = `https://delirius-apiofc.vercel.app/tools/country?text=${PhoneNumber('+' + m.sender.replace('@s.whatsapp.net', '')).getNumber('international')}`
   let response = await fetch(api);
+  let json = await response.json();
   let paisdata = json.result
-  let crow = paisdata ? `${paisdata.emoji}` : 'Desconocido'
+ // let crow = paisdata ? `${paisdata.emoji}` : 'Desconocido'
 
   const customEmoji = global.db.data.chats[m.chat]?.customEmoji || '💛';
   m.react(customEmoji);
@@ -21,7 +22,7 @@ const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, co
   const oi = `*» INFO :* ${pesan}`;
   let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝐇𝐍 𝐄𝐋𝐃𝐄𝐑 𝐁𝐎𝐓 🤖 ꒱  ۟  𝅄 ┄\n`;
   for (const mem of participants) {
-    teks += `┊${paisdata.emoji} @${mem.id.split('@')[0]}\n`;
+    teks += `┊${json.emoji} @${mem.id.split('@')[0]}\n`;
   }
   teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄ ⸼`;
 
