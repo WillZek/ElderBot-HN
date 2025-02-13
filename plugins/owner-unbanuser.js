cff198498fa0c98621e8ecd03c94831acdaa760b
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-13 15:09:06 -0600

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -14,7 +14,7 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
         db[user].banRazon = '';
         const nametag = await conn.getName(user);
         const nn = conn.getName(m.sender);
-        await conn.reply(m.chat, `✅️ El usuario *${nametag}* ha sido desbaneado.`, m, { mentionedJid: [user] });
+        await conn.reply(m.chat, `✨ El usuario *${nametag}* ha sido desbaneado.`, m, { mentionedJid: [user] });
         conn.reply('5493876432076@s.whatsapp.net', `🚩 El usuario *${nametag}* ha sido desbaneado por *${nn}*`, m, rcanal, );
     } else {
         await conn.reply(m.chat, `🚩 El usuario no está registrado.`, m);
