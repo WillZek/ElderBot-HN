b9b8dd0a9c083231091e8f80bb121547eb321783
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-14 10:33:28 -0600

    Actualizar main-donar.js

--- plugins/main-donar.js
+++ plugins/main-donar.js
@@ -6,16 +6,15 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 puedes donar si quieres ayudar a mantener el bot activo
 
 ▢ *PayPal*
-• *Link :* https://www.paypal.me/starcoreNinoPina
+• *Link :* ()
 `
 let img = 'https://i.ibb.co/37FP2bk/donate.jpg'
-conn.sendFile(m.chat, img, 'img.jpg', don, m, null, rcanal)
+conn.sendFile(m.chat, img, 'img.jpg', don, m, null)
 //conn.sendPayment(m.chat, '2000', 'USD', don, m.sender, m)
 }
 
 handler.help = ['donate']
 handler.tags = ['main']
 handler.command = ['apoyar', 'donate', 'donar'] 
-handler.estrellas = 3;
 
 export default handler
