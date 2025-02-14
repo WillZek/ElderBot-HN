5523b8d1422156937d12857d58c8fcaae8e8a5db
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-14 10:17:44 -0600

    Actualizar emox-r34.js

--- plugins/emox-r34.js
+++ plugins/emox-r34.js
@@ -4,7 +4,7 @@ if (!args[0]) {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) {
     return m.reply('[❗] 𝐋𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 +𝟏𝟖 𝐞𝐬𝐭𝐚́𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬 𝐞𝐧 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨.\n> 𝐬𝐢 𝐞𝐬 𝐚𝐝𝐦𝐢𝐧 𝐲 𝐝𝐞𝐬𝐞𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨𝐬 𝐮𝐬𝐞 .enable nsfw');
     }
-await conn.reply(m.chat, '🚩 Ingresa el nombre de la imágen que estas buscando', m, rcanal);
+await conn.reply(m.chat, '🚩 Ingresa el nombre de la imágen que estas buscando', m);
 return;
 }
 const use = args[0];
@@ -14,18 +14,17 @@ conn.reply(m.chat, wait, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: botname,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
+previewType: 0, thumbnail: icons }}})
 const response = await fetch(url);
 const data = await response.json();
 if (!data || data.length === 0) {
-await conn.reply(m.chat, `🚩 No hubo resultados para *${use}*`, m, rcanal);
+await conn.reply(m.chat, `🚩 No hubo resultados para *${use}*`, m);
 return;
 }
 const randomIndex = Math.floor(Math.random() * data.length);
 const randomImage = data[randomIndex];
 const urlimg = randomImage.file_url;
-await conn.sendFile(m.chat, urlimg, 'thumbnail.jpg', `*Resultados De:* ${use}`, m, null, rcanal)
+await conn.sendFile(m.chat, urlimg, 'thumbnail.jpg', `*Resultados De:* ${use}`, m, null)
 } catch (error) {
 console.error(error);
 await m.reply('🚩 Ocurrió un error.');
@@ -34,5 +33,5 @@ handler.help = ['r34 <texto>'];
 handler.command = ['r34', 'rule34'];
 handler.tags = ['emox'];
 handler.register = false;
-// handler.estrellas = 2;
+
 export default handler;
