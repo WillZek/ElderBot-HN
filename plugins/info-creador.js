7b4a55ea58eb42ce5c32aa8bd4efba50c08410b2
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-13 19:29:24 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
     // VCARD
     let list = [{
         displayName: "Elder-Ofc 🎩",
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: ᴇʟᴅᴇʀ-ᴏғɪᴄɪᴀʟ🍭\nitem1.TEL;waid=50489115621:50489115621\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: posadaselder2806@gmail.com\nitem2.X-ABLabel:Email\nitem3.X-ABLabel:Internet\nitem4.ADR:;; Nicaragua;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: ᴇʟᴅᴇʀ-ᴏғɪᴄɪᴀʟ👻\nitem1.TEL;waid=50489115621:50489115621\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: posadaselder2806@gmail.com\nitem2.X-ABLabel:Email\nitem3.X-ABLabel:Internet\nitem4.ADR:;; Nicaragua;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
     }];
 
     await conn.sendMessage(m.chat, {
