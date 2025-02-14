1037df38740d5c1716ab0604425be12c2cdc2eb1
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-14 12:48:42 -0600

    Actualizar owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,28 +1,32 @@
-let handler = async (m, { conn, args, text}) => {
+let handler = async (m, { conn, args, text }) => {
+    let who;
+    if (m.isGroup) {
+        who = m.mentionedJid[0];
+    } else {
+        who = m.chat;
+    }
 
-let cwho = m.mentionedJid[0]
-let who
-if (m.isGroup) who = m.mentionedJid[0]
-else who = m.chat
-
-let user;
-if (m.quoted) {
-user = m.quoted.sender;
-} else if (args.length >= 1) {
-user = args[0].replace('@', '') + '@s.whatsapp.net';
-} else {
+    let user;
+    if (m.quoted) {
+        user = m.quoted.sender;
+    } else if (args.length >= 1) {
+        user = args[0].replace('@', '') + '@s.whatsapp.net';
+    } else {
         await conn.reply(m.chat, '🎩 *Ingresa el @tag de algún usuario.*', m);
         return;
     }
 
-/* if (!who && !m.quoted)
-return m.reply('🎩 *Ingresa el @tag de algún usuario.*');
-*/
+    let users = global.db.data.users;
+    
+    if (!users[who]) {
+        await conn.reply(m.chat, '🎩 *El usuario no existe en la base de datos.*', m);
+        return;
+    }
 
-let users = global.db.data.users
-users[who].banned = true
-conn.reply(m.chat, `✨ *El usuario @${who.split('@')[0]}, fue baneado con éxito.*`, fkontak, { mentions: [who]})
+    users[who].banned = true;
+    conn.reply(m.chat, `✨ *El usuario @${who.split('@')[0]}, fue baneado con éxito.*`, fkontak, { mentions: [who] });
 }
+
 handler.help = ['banuser <@tag> <razón>']
 handler.command = ['banuser']
 handler.tags = ['owner']
