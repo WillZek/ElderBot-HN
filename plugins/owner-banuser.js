a4d2cf3d34ea6adc0491511683d079ab3d942a22
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-13 20:40:04 -0600

    Actualizar owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -3,8 +3,10 @@ if (!text) return m.reply('🍭 *Ingresa el @tag de algún usuario.*')
 let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
-if (!who && !m.quoted) { 
-return m.reply('🎩 *Ingresa el @tag de algún usuario.*')
+if (!who && !m.quoted) {
+return m.reply('🎩 *Ingresa el @tag de algún usuario.*');
+}
+
 let users = global.db.data.users
 users[who].banned = true
 conn.reply(m.chat, `✨ *El usuario @${who.split('@')[0]}, fue baneado con éxito.*`, fkontak, { mentions: [who]})
