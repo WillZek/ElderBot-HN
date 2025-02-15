200c3d1cfdc9c091889736e29457e6a4ef0dcda7
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-15 09:06:31 -0600

    Actualizar owner-exec.js

--- plugins/owner-exec.js
+++ plugins/owner-exec.js
@@ -39,7 +39,7 @@ let handler = async (m, _2) => {
 }
 handler.help = ['> ', '=> ']
 handler.tags = ['owner']
-handler.customPrefix = /^=?> /
+handler.customPrefix = /^=> /
 handler.command = /(?:)/i
 handler.rowner = true
 
