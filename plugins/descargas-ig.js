151809c6ca7dbdd5f98d9f0c6519c6be280fe241
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-15 09:25:17 -0600

    Actualizar descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -23,7 +23,6 @@ conn.reply(m.chat, '⚙️ Ocurrió un error.', m, fake)}}
 handler.command = ['instagram', 'ig']
 handler.tags = ['descargas']
 handler.help = ['instagram', 'ig']
-handler.estrellas = 8;
 handler.group = true;
 handler.register = false
 
