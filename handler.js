21f7330e934d03411841e4f264656862a0e5a3be
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-14 19:52:08 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -503,8 +503,8 @@ const ownerNumber = global.owner[i][0];
 
 global.dfail = (type, m, conn) => {
 const msg = {
-rowner: '「🥷」 *Esta función solo puede ser usada por mi creador ${ownerNumber}*', 
-owner: '「🥷」 *Esta función solo puede ser usada por mi desarrollador. ${ownerNumber}*', 
+rowner: '「🥷」 *Esta función solo puede ser usada por mi creador.*', 
+owner: '「🥷」 *Esta función solo puede ser usada por mi desarrollador.*', 
 mods: '「🥷」 *Esta función solo puede ser usada los moderadores del bot*', 
 premium: '「🥷」 *Esta función solo es para usuarios Premium.*', 
 group: '「🥷」 *Esta funcion solo puede ser ejecutada en grupos.*', 
