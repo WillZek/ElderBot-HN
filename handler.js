524d13417aa90ed08a446217135391b57bef4181
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-02-14 21:07:13 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -498,9 +498,6 @@ this.copyNForward(msg.chat, msg).catch(e => console.log(e, msg))
 console.error(e)
 }}
 
-for (let i = 0; i < global.owner.length; i++) {
-const ownerNumber = global.owner[i][0];
-
 global.dfail = (type, m, conn) => {
 const msg = {
 rowner: '「🥷」 *Esta función solo puede ser usada por mi creador.*', 
