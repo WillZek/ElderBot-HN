6ffc56880db79302ce3e31c38d30a0e8489f405d
Elder504 <elderposadas028@gmail.com>
2025-03-30 22:04:35 -0600

    Actualizar _autodetec.js

--- plugins/_autodetec.js
+++ plugins/_autodetec.js
@@ -12,7 +12,7 @@ nombre = `*El usuario: ${usuario}*\n✦ Ha realizado un cambio en el nombre del 
 foto = `*El usuario ${usuario} ha realizado un cambio en la imagen en el grupo*\n✦ *La nueva imagen es:*`
 edit = `*El ${usuario}*\n✦ Ha configurado que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo`
 newlink = `✦ El enlace de este fue restablecido acción hecha por:\n*» ${usuario}*`
-status = `✦ El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado*' : '*abierto*'} Por *${usuario}*\n\n✦ Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje`
+status = `✦ El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado 🔒*' : '*abierto 🔓*'} Por *${usuario}*\n\n✦ Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje`
 admingp = `*El usuario @${m.messageStubParameters[0].split`@`[0]} Es el nuevo administrador del grupo*\n\n*Acción realizada por: ${usuario}*`
 noadmingp =  `*El usuario @${m.messageStubParameters[0].split`@`[0]} deja de ser administrador de etse grupo*\n\n*Acción realizada por: ${usuario}*`
 
