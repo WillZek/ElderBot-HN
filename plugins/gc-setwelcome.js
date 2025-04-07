8b4b09fafdd378e1cdb0311d025f4459c984bcc9
Elder504 <elderposadas028@gmail.com>
2025-04-07 11:55:54 -0600

    Actualizar gc-setwelcome.js

--- plugins/gc-setwelcome.js
+++ plugins/gc-setwelcome.js
@@ -1,30 +1,13 @@
-/* Código Hecho Por WillZek para CrowBot
-- Tienen Que Definir El welcomeMessage también en el _welcome.js o no funcionará 
-- también tienen que agregar ${welcomeMessage} en el mensaje de bienvenida, eso sí, las comillas son `
-- Espero Les Funcione uwu 
-*/
-
 let handler = async (m, { conn, text, isRowner }) => {
   if (!text) {
-    return m.reply('🎩 Debes proporcionar el mensaje de bienvenida después del comando. Ejemplo: `.setwelcome ¡Bienvenido al grupo!`');
+    return m.reply('*Opciones de bienvenida* 🍃 \n\n*+tag Menciona al usuario*\n\n*+description Descripcion del grupo*');
   }
 
-  const welcomeMessage = text.trim();
-
-  try {
-    global.db.data.chats[m.chat].welcomeMessage = welcomeMessage;
-
-    m.reply(`🎩 El mensaje de bienvenida del grupo ha sido actualizado correctamente a: ${welcomeMessage}`);
-  } catch (error) {
-    console.error(error);
-    m.reply('🎩 Hubo un error al intentar cambiar el mensaje de bienvenida.');
-  }
+  global.welcome = text;
+  m.reply('*✦ El mensaje de bienvenida fue cambiado.*');
 };
 
-handler.help = ['setwelcome *<mensaje>*'];
-handler.tags = ['grupo'];
-handler.command = ['setwelcome', 'setwel'];
-handler.admin = true;
-handler.group = true;
+handler.command = ['setwelcome'];
+handler.isAdmin = true;
 
 export default handler;
