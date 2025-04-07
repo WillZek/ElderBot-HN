e0eee78d23396d1316108c08c59c09ef4380b7c4
Elder504 <elderposadas028@gmail.com>
2025-04-07 12:38:37 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,7 +4,7 @@ import sharp from 'sharp';
 
 export async function before(m, { conn, participants, groupMetadata }) {
   console.log('Evento de grupo recibido:', m.messageStubType, m.isGroup);
-
+  
   if (!m.messageStubType || !m.isGroup) return !0;
 
   let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60');
@@ -14,28 +14,21 @@ export async function before(m, { conn, participants, groupMetadata }) {
   console.log('Configuración del chat:', chat);
 
   if (chat.welcome && m.messageStubType == 27) {
-    let userId = m.messageStubParameters[0];
-    let userName = userId.split('@')[0];
-    let welcome = `
-      🌟 ¡Bienvenido/a al grupo ${groupMetadata.subject}! 🌟
-      
-      👤 Usuario: @${userName}
-      📜 Descripción: ${groupMetadata.desc || 'Sin descripción'}
-      
-      ¡Esperamos que disfrutes tu estadía! 😊
-    `;
+    let welcome = global.welcome
+      .replace('+tag', `@${m.messageStubParameters[0].split('@')[0]}`)
+      .replace('+description', groupMetadata.desc || 'Sin descripción');
 
     console.log('Mensaje de bienvenida:', welcome);
 
     try {
       const extendedImage = await sharp(img)
-        .resize({ width: 1800, height: 700, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
+        .resize({ width: 1200, height: 1200, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
         .toBuffer();
 
-      await conn.sendMessage(m.chat, { image: extendedImage, caption: welcome, mentions: [userId] });
+      await conn.sendMessage(m.chat, { image: extendedImage, caption: welcome });
     } catch (error) {
       console.error('Error al procesar la imagen:', error);
-      await conn.sendMessage(m.chat, { image: img, caption: welcome, mentions: [userId] });
+      await conn.sendMessage(m.chat, { image: img, caption: welcome });
     }
   }
 }
