e3ee70116138a94c6f9c9f5ff627f01827da989f
Elder504 <elderposadas028@gmail.com>
2025-04-07 14:18:05 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,28 +1,26 @@
 import { WAMessageStubType } from '@whiskeysockets/baileys';
-import fetch from 'node-fetch';
-import sharp from 'sharp';
 
 export async function before(m, { conn, participants, groupMetadata }) {
+  console.log('Evento de grupo recibido:', m.messageStubType, m.isGroup);
+  
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60');
-  let img = await (await fetch(pp)).buffer();
 
+  let who = m.messageStubParameters[0];
   let chat = global.db.data.chats[m.chat];
+  console.log('Configuración del chat:', chat);
 
-  if (chat.welcome && m.messageStubType == 27) {
+  // Verifica si el mensaje de bienvenida está habilitado y si el tipo de mensaje es de agregar participante
+  if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
     let welcome = global.welcome
-      .replace('+tag', `@${m.messageStubParameters[0].split('@')[0]}`)
+      .replace('+tag', `@${who.split`@`[0]}`)
       .replace('+description', groupMetadata.desc || 'Sin descripción');
 
-    try {
-      const extendedImage = await sharp(img)
-        .resize({ width: 1800, height: 700, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
-        .toBuffer();
+    console.log('Mensaje de bienvenida:', welcome);
 
-      await conn.sendMini(m.chat, redes, dev, welcome, extendedImage, extendedImage, redeshost);
+    try {
+      await conn.sendMessage(m.chat, { text: welcome, mentions: [who] }, { quoted: m });
     } catch (error) {
-      console.error('Error', error);
-      await conn.sendMini(m.chat, redes, dev, welcome, img, img, redeshost);
+      console.error('Error al enviar mensaje de bienvenida:', error);
     }
   }
 }
