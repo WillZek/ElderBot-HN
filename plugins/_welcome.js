a30220c9713ea11bb228b42eef06a8d6c0c5e49e
Elder504 <elderposadas028@gmail.com>
2025-04-07 14:27:05 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,24 +1,28 @@
 import { WAMessageStubType } from '@whiskeysockets/baileys';
+import fetch from 'node-fetch';
 
 export async function before(m, { conn, participants, groupMetadata }) {
-  console.log('Evento de grupo recibido:', m.messageStubType, m.isGroup);
-  
   if (!m.messageStubType || !m.isGroup) return !0;
 
-  let who = m.messageStubParameters[0];
+  // Define la URL de la imagen por defecto
+  const defaultImage = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60';
+  
+  // Obtén la URL de la imagen de perfil
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => defaultImage);
+  
+  // Descarga la imagen de perfil
+  let image = await (await fetch(pp)).buffer();
+
   let chat = global.db.data.chats[m.chat];
-  console.log('Configuración del chat:', chat);
 
   // Verifica si el mensaje de bienvenida está habilitado y si el tipo de mensaje es de agregar participante
   if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-    let welcome = global.welcome
-      .replace('+tag', `@${who.split`@`[0]}`)
-      .replace('+description', groupMetadata.desc || 'Sin descripción');
-
-    console.log('Mensaje de bienvenida:', welcome);
+    let user = `@${m.messageStubParameters[0].split`@`[0]}`;
+    let description = groupMetadata.desc ? `\n${groupMetadata.desc}` : '';
+    let welcome = `*Usuario ✦ ${user}*\n${description}`;
 
     try {
-      await conn.sendMessage(m.chat, { text: welcome, mentions: [who] }, { quoted: m });
+      await conn.sendMessage(m.chat, { text: welcome, mentions: [m.messageStubParameters[0]] }, { quoted: m });
     } catch (error) {
       console.error('Error al enviar mensaje de bienvenida:', error);
     }
