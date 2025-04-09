610c6e39f1ba7830d6f1026ecd10c7c139e2ee39
Elder504 <elderposadas028@gmail.com>
2025-04-09 11:48:30 -0600

    Actualizar ff-4vs4.js

--- plugins/ff-4vs4.js
+++ plugins/ff-4vs4.js
@@ -3,15 +3,12 @@
 let handler = async(m, { conn }) => {
 
 let vs = `*𝗩𝗔𝗠𝗢𝗦 𝗔 𝗖𝗢𝗠𝗣𝗘*\n\n`
-
     vs += `🛡️ 𝐄𝐒𝐂𝐔𝐀𝐃𝐑𝐀:\n`
     vs += `👑 ┇\n🥷🏻 ┇\n🥷🏻 ┇\n🥷🏻 ┇\n`
     vs += `💫 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:\n`
     vs += `🥷🏻 ┇\n🥷🏻 ┇\n`
-
     vs += `🍨 ${dev}`
 
-let vsimg = 'atbox.moe/ev7gjp.jpg';
 
 conn.sendMessage(m.chat, { image: { url: vsimg }, caption: vs }, { quoted: m });
 }
